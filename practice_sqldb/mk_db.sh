a6b83f1f35d8f08e8afa7f384fd9afb01fc9c6f6
timdavidlee <tim.david.lee@gmail.com>
2019-02-19 22:46:57 -0800

    added the northwind database

---
+++ practice_sqldb/mk_db.sh
@@ -0,0 +1,13 @@
+# from https://github.com/pthom/northwind_psql.git
+#!/bin/bash
+
+dropdb --if-exists northwind
+dropuser --if-exists northwind_user
+
+createdb northwind
+psql northwind < northwind.sql
+
+psql template1 -c "create user northwind_user;"
+psql template1 -c "alter user northwind_user password 'thewindisblowing';"
+psql template1 -c "grant all on DATABASE northwind to northwind_user;"
+psql northwind -c "GRANT ALL on ALL tables IN SCHEMA public to northwind_user"
