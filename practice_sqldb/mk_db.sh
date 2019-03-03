2276f4b0d3ddc5fecf052272c33bd9c218929de1
timdavidlee <tim.david.lee@gmail.com>
2019-03-02 19:21:10 -0800

    updating the sql related questions

--- practice_sqldb/mk_db.sh
+++ practice_sqldb/mk_db.sh
@@ -1,11 +1,13 @@
-# from https://github.com/pthom/northwind_psql.git
+#! from https://github.com/pthom/northwind_psql.git
 #!/bin/bash
 
 dropdb --if-exists northwind
 dropuser --if-exists northwind_user
 
+
+
 createdb northwind
-psql northwind < northwind.sql
+psql northwind < /docker-entrypoint-initdb.d/northwind.sql
 
 psql template1 -c "create user northwind_user;"
 psql template1 -c "alter user northwind_user password 'thewindisblowing';"
